9e520690b57aa6cbb19ac0941572e624230eb7f4
Christopher Hercules <chriherc215@gmail.com>
2024-10-01 21:29:55 -0600

    greeting step

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+string secret = "abfc"; // Hardcoded secret string (can be changed)
+int secretLength = secret.Length;
+
+Console.WriteLine($"Hello, I have chosen {secretLength} letters between 'a' and 'g' and have arranged them in a particular order.");
+Console.WriteLine("Your job is to guess the letters and put them in the right order.");
